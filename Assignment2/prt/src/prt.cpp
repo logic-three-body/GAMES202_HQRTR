4f3f595f0492363b37b41d8c92849c6ece49dd62
logic-three-body <1552451580@qq.com>
2021-04-27 16:30:19 +0800

    尝试法线和光线点乘

--- Assignment2/prt/src/prt.cpp
+++ Assignment2/prt/src/prt.cpp
@@ -224,9 +224,17 @@ public:
                 {
                     // TODO: here you need to calculate shadowed transport term of a given direction
                     // TODO: 此处你需要计算给定方向下的shadowed传输项球谐函数值
-					Ray3f ray(v,wi);
+					//Ray3f ray(v,wi);
 					double vis = 0.0;
-					if (scene->rayIntersect(ray))
+					//if (scene->rayIntersect(ray))
+					//{
+					//	vis = 0.0;
+					//}
+					//else
+					//{
+					//	vis = 1.0;
+					//}
+					if (wi.dot(n)<=0)
 					{
 						vis = 0.0;
 					}
